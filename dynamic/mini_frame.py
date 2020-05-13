770bcd26bf96540b52fa48d5245c22e83bf797c0
叶中强 <18839136833@163.com>
2020-05-13 16:51:45 +0800

    让路由支持正则

--- dynamic/mini_frame.py
+++ dynamic/mini_frame.py
@@ -24,7 +24,7 @@ def route(url):
     return set_func
 
 
-@route("/index.html")
+@route(r"/index.html")
 def index():
     with open("./templates/index.html") as f:
         content = f.read()
@@ -50,7 +50,7 @@ def index():
         <td>%s</td>
         <td>%s</td>
         <td>
-            <input type="button" value="添加" id="toAdd" name="toAdd" systemidvaule="000007">
+            <input type="button" value="添加" id="toAdd" name="toAdd" systemidvaule="%s">
         </td>
         </tr>
     """
@@ -58,7 +58,8 @@ def index():
     html = ""
     for line_info in stock_infos:
         html += tr_template % (
-        line_info[0], line_info[1], line_info[2], line_info[3], line_info[4], line_info[5], line_info[6], line_info[7])
+        line_info[0], line_info[1], line_info[2], line_info[3], line_info[4], line_info[5], line_info[6], line_info[7],
+        line_info[1])
 
     # content = re.sub(r"\{%content%\}", str(stock_infos), content)
     content = re.sub(r"\{%content%\}", html, content)
@@ -66,7 +67,7 @@ def index():
     return content
 
 
-@route("/center.html")
+@route(r"/center.html")
 def center():
     with open("./templates/center.html") as f:
         content = f.read()
@@ -112,6 +113,14 @@ def center():
     return content
 
 
+# 给路由添加正则表达式的原因：在实际开发时，url中往往会带有很多参数，例如/add/000007.html中000007就是参数，
+# 如果没有正则的话，那么就需要编写N次@route来进行添加 url对应的函数 到字典中，此时字典中的键值对有N个，浪费空间
+# 而采用了正则的话，那么只要编写1次@route就可以完成多个 url例如/add/00007.html /add/000036.html等对应同一个函数，此时字典中的键值对个数会少很多
+@route(r"/add/\d+\.html")
+def add_focus():
+    return "add  ok ...."
+
+
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
 
@@ -130,6 +139,20 @@ def application(env, start_response):
     try:
         # func = URL_FUNC_DICT[file_name]
         # return func()
-        return URL_FUNC_DICT[file_name]()
+        # return URL_FUNC_DICT[file_name]()
+        for url, func in URL_FUNC_DICT.items():
+            # {
+            #   r"/index.html":index,
+            #   r"/center.html":center,
+            #   r"/add/\d+\.html":add_focus
+            # }
+            ret = re.match(url, file_name)
+            if ret:
+                return func()
+        else:
+            return "请求的url(%s)没有对应的函数...." % file_name
+
+
     except Exception as ret:
         return "产生了异常：%s" % str(ret)
+
